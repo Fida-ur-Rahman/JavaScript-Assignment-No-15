26bedf699fd765084cd0224514d9a7c4aebed264
Fida-ur-Rahman <fidasaba15@gmail.com>
2025-12-28 23:46:13 +0500

    JavaScript Assignment No 15

---
+++ arr.js
@@ -0,0 +1,13 @@
+// MAP MATHOD
+let arr1 = [12, 34, 45, 56, 67, 90];
+arr1.map((arr1)=>{
+    console.log(arr1)
+});
+
+// FILTER METHOD
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let evenval = arr2.filter((arr2)=>{
+    return arr2 % 2 == 0;
+});
+console.log(arr2);
+console.log(evenval);
